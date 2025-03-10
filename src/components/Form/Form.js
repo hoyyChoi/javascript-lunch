5d3015ec0f9d7a998ec2ba28c112df47bbf6d51f
hoyyChoi <hoyeon8165@gmail.com>
2025-03-10 17:48:34 +0900

    refactor: MODAL_BUTTONS_PROPERTY 값 이동 및 Form 인자로 전달

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,14 +1,13 @@
-import { MODAL_BUTTONS_PROPERTY } from "../../contants.js";
 import ButtonsForm from "./ButtonsForm.js";
 import FormItem from "./FormItem.js";
 
-function Form(formsProperty) {
+function Form(formsProperty, buttonsProperty) {
   const formElement = document.createElement("form");
 
   formsProperty.forEach((formProperty) => {
     formElement.appendChild(FormItem(formProperty));
   });
-  formElement.appendChild(ButtonsForm(MODAL_BUTTONS_PROPERTY));
+  formElement.appendChild(ButtonsForm(buttonsProperty));
 
   return formElement;
 }
