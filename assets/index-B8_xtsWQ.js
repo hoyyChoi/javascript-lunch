3dad9ef97b8ca3bb909a841acd1db43f8f6ae643
hoyyChoi <hoyeon8165@gmail.com>
2025-03-17 11:30:25 +0900

    Updates

--- assets/index-B8_xtsWQ.js
+++ assets/index-B8_xtsWQ.js
@@ -548,7 +548,7 @@ const MODAL_FORM_CONFIG = {
       label: "참고 링크",
       type: "input",
       name: "link",
-      inputType: "text",
+      inputType: "url",
       notice: "매장 정보를 확인할 수 있는 링크를 입력해 주세요."
     }
   ],
